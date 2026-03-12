f85d9d5d625d2d8dc032c65064984798d09f44df
Dmitry Romanov <romanovda@gmail.com>
2026-03-12 13:51:45 -0400

    Add conversion shell ifarm script

---
+++ csv_convert/convert_combinatorics.18x275.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=eic
+#SBATCH --partition=production
+#SBATCH --job-name=MSCSV18x275
+#SBATCH --time=1-00:00:00          # format days-hours:minutes:seconds
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --output=/volatile/eic/romanov/meson-structure-2025-10/convert_csv_18x275.%j.log
+#SBATCH --error=/volatile/eic/romanov/meson-structure-2025-10/convert_csv_18x275.%j.err
+
+set -euo pipefail
+
+CAMPAIGN=/volatile/eic/romanov/meson-structure-2026-03/dd4hep/18x275-priority
+CSV_CONVERT_DIR=/work/eic/users/romanov/meson-structure-work/meson-structure/csv_convert
+IMG=/cvmfs/singularity.opensciencegrid.org/eicweb/eic_xl:nightly
+
+echo "[INFO]  Running root in $(hostname)"
+echo "[INFO]  Campaign directory : $CAMPAIGN"
+echo "[INFO]  Container image    : $IMG"
+echo "[INFO]  CSV_CONVERT_DIR    : $CSV_CONVERT_DIR"
+
+
+# Bind the whole campaign so script sees reco/ inside /work
+singularity exec -B "$CAMPAIGN":/work -B "$CSV_CONVERT_DIR":/code "$IMG" \
+   bash -c 'cd /code &&root -x -l -b -q '\''csv_edm4hep_ppim_combinatorics.cxx("/work/k_lambda_18x275_5000evt_0001.edm4hep.root", "18x275_combinatorics.csv", 5000)'\'''
+
+echo "[DONE]  Conversion job finished"
