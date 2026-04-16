7b5e3a3e84fba76b2cf8b27eee1cba4c3a2ed1c6
Dmitry Romanov <romanovda@gmail.com>
2026-04-15 21:43:55 -0400

    Ifarm analysis scripts

--- analysis/edm4hep_lambda/analysis_26-03_saveall.10x100.sh
+++ analysis/edm4hep_lambda/analysis_26-03_saveall.10x100.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --account=eic
 #SBATCH --partition=production
-#SBATCH --job-name=MS-2026-03
+#SBATCH --job-name=MS-26-03-sa
 #SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
-#SBATCH --output=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_2026-03_10x100.%j.log
-#SBATCH --error=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_2026-03_10x100.%j.err
+#SBATCH --output=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_26-03_saveall_10x100.%j.log
+#SBATCH --error=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_26-03_saveall_10x100.%j.err
 
 set -euo pipefail
 
-CAMPAIGN=/work/eic3/users/romanov/meson-structure-2026-04-check/dd4hep_2026-03/10x100
+CAMPAIGN=/work/eic3/users/romanov/meson-structure-2026-04-check/dd4hep_26-03_saveall/10x100
 OUTPUT_DIR=/work/eic3/users/romanov/meson-structure-2026-04-check
 CODE_DIR=/work/eic/users/romanov/meson-structure-work/meson-structure/analysis/edm4hep_lambda
 IMG=/cvmfs/singularity.opensciencegrid.org/eicweb/eic_xl:26.03.1-stable
@@ -26,6 +26,6 @@ singularity exec \
     -B "$CODE_DIR":/code \
     -B "$OUTPUT_DIR":/output \
     "$IMG" \
-    bash -c 'cd /code && root -x -l -b -q '"'"'mcpart_lambda.cxx("/work/*.edm4hep.root", "/output/ana_2026-03_10x100")'"'"''
+    bash -c 'cd /code && root -x -l -b -q '"'"'mcpart_lambda.cxx("/work/*.edm4hep.root", "/output/ana_26-03_saveall_10x100")'"'"''
 
 echo "[DONE]  Job finished"
--- analysis/edm4hep_lambda/analysis_26-04_saveall.10x100.sh
+++ analysis/edm4hep_lambda/analysis_26-04_saveall.10x100.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH --account=eic
 #SBATCH --partition=production
-#SBATCH --job-name=MS-2026-03
+#SBATCH --job-name=MS-26-04-sa
 #SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
-#SBATCH --output=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_2026-03_10x100.%j.log
-#SBATCH --error=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_2026-03_10x100.%j.err
+#SBATCH --output=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_26-04_saveall_10x100.%j.log
+#SBATCH --error=/work/eic3/users/romanov/meson-structure-2026-04-check/ana_26-04_saveall_10x100.%j.err
 
 set -euo pipefail
 
-CAMPAIGN=/work/eic3/users/romanov/meson-structure-2026-04-check/dd4hep_2026-03/10x100
+CAMPAIGN=/work/eic3/users/romanov/meson-structure-2026-04-check/dd4hep_26-04_saveall/10x100
 OUTPUT_DIR=/work/eic3/users/romanov/meson-structure-2026-04-check
 CODE_DIR=/work/eic/users/romanov/meson-structure-work/meson-structure/analysis/edm4hep_lambda
-IMG=/cvmfs/singularity.opensciencegrid.org/eicweb/eic_xl:26.03.1-stable
+IMG=/cvmfs/singularity.opensciencegrid.org/eicweb/eic_xl:nightly
 
 echo "[INFO]  Running root in $(hostname)"
 echo "[INFO]  Campaign directory : $CAMPAIGN"
@@ -26,6 +26,6 @@ singularity exec \
     -B "$CODE_DIR":/code \
     -B "$OUTPUT_DIR":/output \
     "$IMG" \
-    bash -c 'cd /code && root -x -l -b -q '"'"'mcpart_lambda.cxx("/work/*.edm4hep.root", "/output/ana_2026-03_10x100")'"'"''
+    bash -c 'cd /code && root -x -l -b -q '"'"'mcpart_lambda.cxx("/work/*.edm4hep.root", "/output/ana_26-04_saveall_10x100")'"'"''
 
 echo "[DONE]  Job finished"
